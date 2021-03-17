8bf7ace01bb3e10cc691d69832f6149ae3998a6a
Suganya <suganprakash0621@gmail.com>
2021-03-16 22:38:39 -0500

    started coding

--- src/Tax_file.js
+++ src/Tax_file.js
@@ -12,3 +12,41 @@
 let Income1 = 10000
 let Income2 = 20000
 let Income3 = 50000
+
+
+function calcTaxes(amount){
+
+    let calculate = 0;
+    if(amount > Income3){
+        tax = (amount -Income3) * .30 + 
+    }
+}
+
+// Google
+var calculate_tax = function() {
+    var total;
+    var income = parseFloat( $("income").value );
+
+    function calcTaxes(amount){
+    var calculate = 0;
+    if(amount > 85650){
+        tax = (amount - 85650) * .28 + 870.0+(35350-8700)*.15+(89350-36900)*.25;
+    }
+    else if( amount > 35350){
+    tax = (amount - 35350) * .25 + 870.0+(35350-8700)*.15;    
+    }
+    else if(amount > 8700){
+    tax = (amount - 8700) * .15 + 870.0;
+    }
+    else{
+       tax = amount * .10;
+        }
+    tax += amount * .153;
+    return tax;
+    /*
+    10% on taxable income from $0 to $8,700, plus
+    15% on taxable income over $8,700 to $35,350, plus
+    25% on taxable income over $35,350 to $85,650, plus
+    */
+}
+}
