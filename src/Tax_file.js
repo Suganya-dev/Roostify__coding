a6cc6c36bcf650a656159d1ab4a1fb5cb73f72e7
Suganya <suganprakash0621@gmail.com>
2021-03-16 22:22:40 -0500

    added some codes

---
+++ src/Tax_file.js
@@ -0,0 +1,14 @@
+// Write a program that calculates income tax based on the following rules:
+// a.The portion of the income that is less than $10k is untaxed
+// b.The portion of the income that is less than $20k is taxed at 10%
+// c.The portion of the income that is less than $50k is taxed at 20%
+// d.Any portion of the income that is above $50k is taxed at 30%
+
+// Assume this application will be used by a 3rd party tax consultant who will have to run this for 100 clients. 
+//  Write a program that is scalable.
+// The program should take the $ income and return the tax amount.
+
+// Declaring the variables using let keyword, so that we can re-assign the values to it.
+let Income1 = 10000
+let Income2 = 20000
+let Income3 = 50000
